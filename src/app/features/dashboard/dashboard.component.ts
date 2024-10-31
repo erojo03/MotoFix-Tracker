9e0e191571c7dc769ee1e1e4eea0cd745cdb21fd
erojo03 <edgar.rojo03@outlook.com>
2024-10-31 13:03:24 -0500

    feat(dashboard): :sparkles: implement basic layout and navigation
    - Add NavbarComponent to the dashboard - Set up responsive flex layout for desktop and mobile - Integrate RouterOutlet for nested routing

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
-  styles: ``,
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    <app-navbar />
+    <main class="flex-1 overflow-y-auto">
+      <router-outlet />
+    </main>
+  `,
+  styles: `
+    :host {
+      @apply flex flex-col-reverse px-4 min-h-screen h-dvh md:px-8 md:flex-col
+    }
+  `,
 })
 export class DashboardComponent {}
