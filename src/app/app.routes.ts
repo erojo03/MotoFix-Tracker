27b0928bbc5cd9b0a9e90cffb0e17fbda6d3525b
erojo03 <edgar.rojo03@outlook.com>
2024-10-31 02:26:14 -0500

    feat(routing): Configured main routing for dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    title: 'Dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+    loadChildren: () => import('./features/dashboard/dashboard.routes'),
+  },
+];
