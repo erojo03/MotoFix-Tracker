82dceb0a842f60a4b6f64dcd2ae5a747d282feb5
erojo03 <edgar.rojo03@outlook.com>
2024-10-30 23:34:10 -0500

    feat(component): Added child components (home, motorcycles, notifications, users) to Dashboard

---
+++ src/app/features/dashboard/motorcycles/motorcycles.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motorcycles',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      motorcycles works!
+    </p>
+  `,
+  styles: ``
+})
+export class MotorcyclesComponent {
+
+}
