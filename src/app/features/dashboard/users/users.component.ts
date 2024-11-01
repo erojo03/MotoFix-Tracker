390ff53bf8ae4adde21d8eae3e678f90bcfdf670
erojo03 <edgar.rojo03@outlook.com>
2024-11-01 02:24:00 -0500

    feat(component): :sparkles: Created Users component, integrating SearchBar component for user filtering

--- src/app/features/dashboard/users/users.component.ts
+++ src/app/features/dashboard/users/users.component.ts
@@ -1,16 +1,61 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../../shared/components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent],
   template: `
-    <p>
-      users works!
-    </p>
-  `,
-  styles: ``
-})
-export class UsersComponent {
+    <!-- Header -->
+    <section class="md:flex md:items-center md:justify-between">
+      <h1>USUARIOS</h1>
+      <div class="flex flex-col gap-2 md:flex-row-reverse">
+        <app-search-bar />
 
-}
+        <!-- Header Buttons -->
+        <div class="flex flex-shrink-0 gap-4">
+          <button
+            type="button"
+            class="flex w-full items-center justify-center gap-1 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 512"
+              class="size-5">
+              <path
+                class="fill-current"
+                d="M224 256C294.695 256 352 198.691 352 128S294.695 0 224 0C153.312 0 96 57.309 96 128S153.312 256 224 256ZM274.664 304H173.336C77.609 304 0 381.602 0 477.332C0 496.477 15.523 512 34.664 512H413.336C432.477 512 448 496.477 448 477.332C448 381.602 370.398 304 274.664 304ZM616 200H568V152C568 138.75 557.25 128 544 128S520 138.75 520 152V200H472C458.75 200 448 210.75 448 224S458.75 248 472 248H520V296C520 309.25 530.75 320 544 320S568 309.25 568 296V248H616C629.25 248 640 237.25 640 224S629.25 200 616 200Z" />
+            </svg>
+            Agregar usuario
+          </button>
+
+          <button
+            class="flex w-full items-center justify-center rounded-lg border border-gray-200 px-4 py-2 text-sm font-medium hover:bg-gray-100 md:w-auto">
+            <svg
+              class="mr-2 h-4 w-4 text-gray-400"
+              viewbox="0 0 20 20"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
+                clip-rule="evenodd" />
+            </svg>
+            Filtrar
+            <svg
+              class="-mr-1 ml-1.5 h-5 w-5"
+              fill="currentColor"
+              viewbox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </section>
+  `,
+  styles: ``,
+})
+export class UsersComponent {}
