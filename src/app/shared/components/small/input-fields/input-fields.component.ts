2b52de435e1cd0c1d3db3570a1d00b8a7e6e81c4
erojo03 <edgar.rojo03@outlook.com>
2024-11-03 01:27:15 -0500

    feat(component): ✨ created inputFields
    - This input is used for forms.

---
+++ src/app/shared/components/small/input-fields/input-fields.component.ts
@@ -0,0 +1,51 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ControlContainer, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface inputField {
+  name: string;
+  label: string;
+  type: string;
+  placeholder: string;
+  mensaje: string;
+}
+
+@Component({
+  selector: 'app-input-fields',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <label [for]="field.name" class="flex flex-col gap-2 text-sm font-medium"
+      >{{ field.label }}
+      <input
+        [id]="field.name"
+        [formControlName]="field.name"
+        [type]="field.type"
+        [placeholder]="field.placeholder"
+        class="w-full rounded-md border px-3 py-1" />
+    </label>
+  `,
+  styles: ``,
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, { skipSelf: true }),
+    },
+  ],
+})
+export class InputFieldsComponent implements OnInit {
+  @Input({ required: true }) field!: inputField;
+
+  private parentContainer = inject(ControlContainer);
+  private _fb = inject(NonNullableFormBuilder);
+
+  ngOnInit(): void {
+    this.parentForm.addControl(
+      this.field.name,
+      this._fb.control('', [Validators.required])
+    );
+  }
+
+  get parentForm() {
+    return this.parentContainer.control as FormGroup;
+  }
+}
