7285a32017b90dda0a2cae9a39032508a2dd87d1
erojo03 <edgar.rojo03@outlook.com>
2024-11-03 01:27:57 -0500

    feat(component): ✨ created selectFields
    - This select is used for forms.

---
+++ src/app/shared/components/small/select-fields/select-fields.component.ts
@@ -0,0 +1,62 @@
+import { Component, inject, Input, input, OnInit } from '@angular/core';
+import {
+  ControlContainer,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+interface Entity {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-select-fields',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <label class="flex flex-col gap-2 text-sm font-medium">
+      {{ title }}
+      <select
+        [formControlName]="controlKey"
+        class="w-full rounded-md border px-3 py-2">
+        <option value="" disabled selected>Seleccionar rol</option>
+        @for (entity of entities(); track entity.id) {
+          <option [value]="entity.id">{{ entity.name }}</option>
+        }
+      </select>
+    </label>
+  `,
+  styles: `
+    :host {
+      grid-column: span 2;
+    }
+  `,
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, { skipSelf: true }),
+    },
+  ],
+})
+export class SelectFieldsComponent implements OnInit {
+  @Input({ required: true }) title = '';
+  @Input({ required: true }) controlKey = '';
+  entities = input<Entity[]>();
+
+  private parentContainer = inject(ControlContainer);
+  private _fb = inject(NonNullableFormBuilder);
+
+  ngOnInit(): void {
+    this.parentForm.addControl(
+      this.controlKey,
+      this._fb.control('', [Validators.required])
+    );
+  }
+
+  get parentForm() {
+    return this.parentContainer.control as FormGroup;
+  }
+}
