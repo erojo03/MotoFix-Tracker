9a5ec2e74c59c08725a95fc66b59b2e5856f29c6
erojo03 <edgar.rojo03@outlook.com>
2024-11-03 18:06:48 -0500

    feat(SubmitButtonComponent): add dynamic text input and change formValid
    - Implement required text input for button content
    - Change formValid input to be optional with a default value of true
    - Add responsive text display (full text on desktop, first word on mobile)
    - Use new Angular input() syntax for text and formValid properties

--- src/app/shared/components/small/submit-button/submit-button.component.ts
+++ src/app/shared/components/small/submit-button/submit-button.component.ts
@@ -9,7 +9,7 @@ import { Component, EventEmitter, input, Output } from '@angular/core';
       (click)="submitEvent()"
       [disabled]="!formValid()"
       type="submit"
-      class="w-full rounded-md bg-blue-600 px-4 py-2 text-white enabled:hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50">
+      class="w-full rounded-md bg-blue-600 px-4 py-2 text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 enabled:hover:bg-blue-700 disabled:opacity-50">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="mr-2 inline-block h-5 w-5"
@@ -22,14 +22,15 @@ import { Component, EventEmitter, input, Output } from '@angular/core';
           stroke-width="2"
           d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
       </svg>
-      <span class="hidden sm:inline">Agregar Usuario</span>
-      <span class="sm:hidden">Agregar</span>
+      <span class="hidden sm:inline">{{ text() }}</span>
+      <span class="sm:hidden">{{ text().split(' ')[0] }}</span>
     </button>
   `,
   styles: ``,
 })
 export class SubmitButtonComponent {
-  formValid = input.required<boolean>();
+  text = input.required<string>();
+  formValid = input<boolean>(true);
   @Output() submitForm = new EventEmitter<void>();
 
   submitEvent(): void {
