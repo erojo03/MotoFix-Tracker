dc0ec3d7d1e594e5a7bc3a78c41190e575c8242d
erojo03 <edgar.rojo03@outlook.com>
2024-11-03 18:14:43 -0500

    feat(component): ✨ Created loginComponent

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,82 @@
+import { Component, inject } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { SubmitButtonComponent } from '../../shared/components/small/submit-button/submit-button.component';
+import { InputFieldsComponent } from '../../shared/components/small/input-fields/input-fields.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, SubmitButtonComponent, InputFieldsComponent],
+  template: `
+    <main class="flex h-dvh flex-col items-center justify-center px-9">
+      <img
+        src="assets/wave-1.svg"
+        alt="wave"
+        class="fixed right-0 top-0 -z-10" />
+      <img
+        src="assets/wave-2.svg"
+        alt="wave"
+        class="fixed bottom-0 left-0 -z-10" />
+
+      <div
+        class="flex w-full max-w-md flex-col items-center justify-center gap-6">
+        <div class="flex w-full flex-col items-center justify-center gap-6">
+          <h1
+            class="shadow-title-login rounded-3xl bg-[#EA5656] px-4 text-center text-[2.5rem] font-bold text-white">
+            BIENVENIDO
+          </h1>
+          <form
+            [formGroup]="logInForm"
+            (ngSubmit)="onSubmit()"
+            class="flex w-full flex-col items-center gap-3">
+            @for (field of fields; track field.name) {
+              <app-input-fields [field]="field" />
+            }
+
+            <button
+              class="from-4% hover:shadow-btn-login-hover shadow-btn-login mt-5 flex w-48 items-center justify-center gap-2 rounded-3xl bg-gradient-to-r from-[#627fdf] to-[#102e6a] to-100% px-4 py-3 font-semibold text-white transition-all duration-300 ease-in-out hover:scale-105 hover:from-[#102e6a] hover:to-[#627fdf]">
+              Iniciar Sesion
+            </button>
+          </form>
+        </div>
+        <div class="">
+          <img src="assets/logo.webp" alt="logo" class="w-56" />
+        </div>
+      </div>
+    </main>
+  `,
+  styles: ``,
+})
+export class LoginComponent {
+  private readonly _fb = inject(NonNullableFormBuilder);
+
+  fields = [
+    {
+      name: 'phone',
+      label: 'Celular',
+      type: 'tel',
+      placeholder: '999999999',
+      mensaje: 'Ingresa un numero correcto',
+    },
+    {
+      name: 'password',
+      label: 'Contraseña',
+      type: 'password',
+      placeholder: '*********',
+      mensaje: 'Ingresa una contraseña valida',
+    },
+  ];
+
+  logInForm = this._fb.group({
+    phone: ['', [Validators.required, Validators.pattern(/^\+[1-9]\d{1,14}$/)]],
+    password: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    console.log(this.logInForm.getRawValue());
+  }
+}
