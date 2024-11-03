d992f5ad0c9f6ad45f1b2a2eb7a23fee1c5ec615
erojo03 <edgar.rojo03@outlook.com>
2024-11-02 20:27:55 -0500

    feat(component): ✨ created submitButton
    - This button is used for forms.

---
+++ src/app/shared/components/small/submit-button/submit-button.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <button
+      (click)="submitEvent()"
+      [disabled]="!formValid()"
+      type="submit"
+      class="w-full rounded-md bg-blue-600 px-4 py-2 text-white enabled:hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="mr-2 inline-block h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+      </svg>
+      <span class="hidden sm:inline">Agregar Usuario</span>
+      <span class="sm:hidden">Agregar</span>
+    </button>
+  `,
+  styles: ``,
+})
+export class SubmitButtonComponent {
+  formValid = input.required<boolean>();
+  @Output() submitForm = new EventEmitter<void>();
+
+  submitEvent(): void {
+    this.submitForm.emit();
+  }
+}
