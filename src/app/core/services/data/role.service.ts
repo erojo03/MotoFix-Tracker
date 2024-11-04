909a72ce71580c4465b75c1798d15977f20cc63e
erojo03 <edgar.rojo03@outlook.com>
2024-11-04 00:07:26 -0500

    refactor: 🏗️ extract Role interface to separate file
    - Move Role interface from role.service.ts to role.interface.ts
    - Update import in role.service.ts to use the new interface file
    - Improve code organization and reusability of the Role interface

--- src/app/core/services/data/role.service.ts
+++ src/app/core/services/data/role.service.ts
@@ -2,11 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
-
-interface Role {
-  id: number;
-  name: string;
-}
+import { Role } from '../../interfaces/role.interface';
 
 @Injectable({
   providedIn: 'root',
