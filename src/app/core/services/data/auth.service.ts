832f133b271fc5f2996a4a8d0231168ca2a0fb52
erojo03 <edgar.rojo03@outlook.com>
2024-11-03 22:33:51 -0500

    feat(auth): 🔒 store access token in local storage after successful login
    - Integrate StorageService in AuthService
    - Save access_token to local storage using StorageService after login
    - Use RxJS tap operator to perform side effect without affecting the stream

--- src/app/core/services/data/auth.service.ts
+++ src/app/core/services/data/auth.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { signUp } from '../../interfaces/auth.interface';
 import { environment } from '../../../../environments/environment.development';
+import { StorageService } from '../utils/storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private _http = inject(HttpClient);
+  private _storageService = inject(StorageService);
 
   signUp(
     firstName: string,
@@ -27,12 +29,15 @@ export class AuthService {
   }
 
   logIn(phone: number, password: string): Observable<{ access_token: string }> {
-    return this._http.post<{ access_token: string }>(
-      `${environment.API_URL}/auth/log-in`,
-      {
+    return this._http
+      .post<{ access_token: string }>(`${environment.API_URL}/auth/log-in`, {
         phone,
         password,
-      }
-    );
+      })
+      .pipe(
+        tap((reponse) => {
+          this._storageService.set('session', reponse.access_token);
+        })
+      );
   }
 }
