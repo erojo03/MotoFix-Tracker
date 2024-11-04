022a488ea48e1a76127076710913f21863b5454e
erojo03 <edgar.rojo03@outlook.com>
2024-11-03 23:43:56 -0500

    feat(service): 👥 add RoleService for role management
    - Create RoleService to fetch and manage user roles
    - Implement getRoles() method to retrieve roles from API
    - Add role name translations for better user experience
    - Use environment configuration for API URL
    - Map API response to translate role names

---
+++ src/app/core/services/data/role.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+
+interface Role {
+  id: number;
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoleService {
+  private _http = inject(HttpClient);
+
+  rolesTranslate: Record<string, string> = {
+    admin: 'Administrador',
+    receptionist: 'Recepcionista',
+    'workshop manager': 'Jefe de Taller',
+    mechanics: 'Mecanico',
+    storekeeper: 'Almacenero',
+    // Agrega más traducciones según sea necesario
+  };
+
+  getRoles(): Observable<Role[]> {
+    return this._http.get<Role[]>(`${environment.API_URL}/roles`).pipe(
+      map((response) =>
+        response.map((role) => {
+          return {
+            id: role.id,
+            name: this.rolesTranslate[role.name] || role.name,
+          };
+        })
+      )
+    );
+  }
+}
