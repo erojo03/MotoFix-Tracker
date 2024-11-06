bc724668c61f72b59fc6b0a5752da411cbdccc91
erojo03 <edgar.rojo03@outlook.com>
2024-11-05 23:55:24 -0500

    refactor(search-bar): 🎨 simplify component structure
    - Remove main div wrapper from template
    - Move wrapper styles to :host selector
    - Improve component encapsulation and reduce DOM nesting

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -5,21 +5,23 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="relative w-full md:max-w-96">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        class="absolute left-3 top-1/2 size-4 -translate-y-1/2 fill-[#DC5054]">
-        <path
-          d="M504.971 448.402L380.623 324.053C402.953 290.902 416 250.977 416 208C416 93.125 322.875 0 208 0S0 93.125 0 208S93.125 416 208 416C250.977 416 290.902 402.953 324.053 380.623L448.402 504.971C457.775 514.344 472.971 514.344 482.344 504.971L504.971 482.344C514.344 472.971 514.344 457.775 504.971 448.402ZM80 208C80 137.42 137.42 80 208 80S336 137.42 336 208C336 278.578 278.58 336 208 336S80 278.578 80 208Z" />
-      </svg>
-      <input
-        type="text"
-        placeholder="Buscar {{ placeholder }}..."
-        class="w-full rounded-lg border border-gray-300 bg-gray-50 p-2 px-8 text-sm text-gray-900" />
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      class="absolute left-3 top-1/2 size-4 -translate-y-1/2 fill-[#DC5054]">
+      <path
+        d="M504.971 448.402L380.623 324.053C402.953 290.902 416 250.977 416 208C416 93.125 322.875 0 208 0S0 93.125 0 208S93.125 416 208 416C250.977 416 290.902 402.953 324.053 380.623L448.402 504.971C457.775 514.344 472.971 514.344 482.344 504.971L504.971 482.344C514.344 472.971 514.344 457.775 504.971 448.402ZM80 208C80 137.42 137.42 80 208 80S336 137.42 336 208C336 278.578 278.58 336 208 336S80 278.578 80 208Z" />
+    </svg>
+    <input
+      type="text"
+      placeholder="Buscar {{ placeholder }}..."
+      class="w-full rounded-lg border border-gray-300 bg-gray-50 p-2 px-8 text-sm text-gray-900" />
   `,
-  styles: ``,
+  styles: `
+    :host {
+      @apply relative w-full md:max-w-96;
+    }
+  `,
 })
 export class SearchBarComponent {
   @Input() placeholder = '';
