369ce21a1cfd83a6a190aafce029fcebd7c0b816
erojo03 <edgar.rojo03@outlook.com>
2024-11-05 20:44:47 -0500

    feat(users): ✨ Create UserEditComponent
    - Create UserEditComponent with form for editing user information
    - Implement form submission and user update functionality
    - Add input fields for first name, last name, phone, and password
    - Include role selection dropdown
    - Integrate with UserService for updating user data

---
+++ src/app/features/dashboard/users/components/user-edit/user-edit.component.ts
@@ -0,0 +1,149 @@
+import { Component, inject, input, OnInit } from '@angular/core';
+import { SubmitButtonComponent } from '../../../../../shared/components/small/submit-button/submit-button.component';
+import { InputFieldsComponent } from '../../../../../shared/components/small/input-fields/input-fields.component';
+import { SelectFieldsComponent } from '../../../../../shared/components/small/select-fields/select-fields.component';
+import { Role } from '../../../../../core/interfaces/role.interface';
+import {
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CloseButtonComponent } from '../../../../../shared/components/small/close-button/close-button.component';
+import { UserList } from '../../../../../core/interfaces/user.interface';
+import { UserService } from '../../../../../core/services/data/user.service';
+
+@Component({
+  selector: 'app-user-edit',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    SubmitButtonComponent,
+    InputFieldsComponent,
+    SelectFieldsComponent,
+    CloseButtonComponent,
+  ],
+  template: `
+    <section
+      class="relative flex w-full flex-col gap-4 rounded-lg bg-white p-6 shadow-xl md:max-w-2xl md:rounded-xl">
+      <app-close-button popupId="editUser" />
+
+      <!-- Title -->
+      <header class="text-center">
+        <h2 class="text-2xl font-bold sm:text-3xl">Editar Usuario</h2>
+        <p class="mt-2 text-sm text-gray-600 sm:text-base">
+          Modifique los detalles del usuario
+        </p>
+      </header>
+
+      <!-- Form -->
+      <form
+        [formGroup]="userForm"
+        (ngSubmit)="onSubmit()"
+        class="grid grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-4">
+        @for (field of fields; track field.name) {
+          <app-input-fields [field]="field" />
+        }
+        <app-select-fields title="Rol" [entities]="roles()" controlKey="role" />
+      </form>
+
+      <!-- Submit Button -->
+      <app-submit-button
+        text="Guardar Cambios"
+        [formValid]="userForm.valid"
+        (submitForm)="onSubmit()" />
+    </section>
+  `,
+  styles: `
+    :host {
+      @apply content-box;
+    }
+  `,
+})
+export class UserEditComponent implements OnInit {
+  user = input<UserList>();
+  roles = input<Role[]>();
+
+  private readonly _fb = inject(NonNullableFormBuilder);
+  private readonly _userService = inject(UserService);
+
+  fields = [
+    {
+      name: 'firstName',
+      label: 'Nombre',
+      type: 'text',
+      placeholder: 'Juan',
+      mensaje: 'Ingresa un nombre correcto',
+    },
+    {
+      name: 'lastName',
+      label: 'Apellido',
+      type: 'text',
+      placeholder: 'Pérez',
+      mensaje: 'Ingresa un apellido correcto',
+    },
+    {
+      name: 'phone',
+      label: 'Celular',
+      type: 'tel',
+      placeholder: '999 999 999',
+      mensaje: 'Ingresa un número de celular válido',
+    },
+    {
+      name: 'password',
+      label: 'Contraseña',
+      type: 'password',
+      placeholder: '••••••••',
+      mensaje: 'Ingresa una contraseña',
+    },
+  ];
+
+  userForm: FormGroup = this._fb.group({});
+
+  ngOnInit(): void {
+    this.setForm();
+  }
+
+  private async setForm(): Promise<void> {
+    const user = this.user();
+    if (user) {
+      setTimeout(() => {
+        this.userForm.patchValue({
+          firstName: user.firstName,
+          lastName: user.lastName,
+          phone: user.phone,
+          role: user.role.id,
+        });
+      }, 0);
+    }
+  }
+
+  onSubmit() {
+    if (this.userForm.invalid) {
+      this.userForm.markAllAsTouched();
+      return;
+    }
+
+    const { firstName, lastName, phone, password, role } =
+      this.userForm.getRawValue();
+
+    try {
+      const user = this.user();
+      if (user) {
+        this._userService
+          .updateUser(
+            user.id,
+            firstName,
+            lastName,
+            phone,
+            password,
+            Number(role)
+          )
+          .subscribe();
+      } else {
+        console.error('User is undefined');
+      }
+    } catch (error) {
+      console.error('Error al actualizar el usuario:', error);
+    }
+  }
+}
