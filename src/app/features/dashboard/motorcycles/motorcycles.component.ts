b6a1c1c1fb040a9cb20576fa757f113be286ac96
erojo03 <edgar.rojo03@outlook.com>
2024-11-06 00:10:44 -0500

    feat(motorcycles): 🏍️ implement header and search bar
    - Add header section with title "ATENCIONES MECANICAS"
    - Display current date using DatePipe
    - Integrate SearchBarComponent for motorcycle search functionality
    - Implement responsive layout for header section
    - Prepare structure for future motorcycle list implementation

--- src/app/features/dashboard/motorcycles/motorcycles.component.ts
+++ src/app/features/dashboard/motorcycles/motorcycles.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../../shared/components/search-bar/search-bar.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-motorcycles',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent, DatePipe],
   template: `
-    <p>
-      motorcycles works!
-    </p>
+    <!-- Header -->
+    <section
+      class="flex flex-col items-center gap-2.5 md:flex-row md:justify-between">
+      <header class="w-full">
+        <h1 class="text-[1.4rem] font-extrabold">ATENCIONES MECANICAS</h1>
+        <time class="font-medium">{{ date | date: 'dd / MM / yyyy' }}</time>
+      </header>
+      <app-search-bar />
+    </section>
+
+    <!-- Motorcycle List -->
+    <section></section>
   `,
-  styles: ``
+  styles: ``,
 })
 export class MotorcyclesComponent {
-
+  date = new Date();
 }
