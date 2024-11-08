3bd62227b955eee9e8e8d3e94382bb9778b89b0f
erojo03 <edgar.rojo03@outlook.com>
2024-11-08 14:22:27 -0500

    feat(ModelService): 🔄 synchronize models signal with API response
    - ✨ Add tap operator to update _models signal after fetching data
    - 🔗 Ensure local state reflects the latest server data
    - 🎯 Improve data consistency across the application

--- src/app/features/dashboard/motorcycles/services/model.service.ts
+++ src/app/features/dashboard/motorcycles/services/model.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Model } from '../interfaces/model.interface';
 import { environment } from '../../../../../environments/environment.development';
 
@@ -18,7 +18,7 @@ export class ModelService {
   getModelsByBrand(brandId: number): Observable<Model[]> {
     return this._http.get<Model[]>(
       `${environment.API_URL}/motorcycle/model/brand/${brandId}`
-    );
+    ).pipe(tap((models) => this._models.set(models)));
   }
 
   addModel(brandId: number, name: string): Observable<Model> {
