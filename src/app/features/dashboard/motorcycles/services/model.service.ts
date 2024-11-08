06d503fb1ed730a4595eb9f3e87018249c048388
erojo03 <edgar.rojo03@outlook.com>
2024-11-08 00:53:12 -0500

    feat(Motorcycles): ✨ Add ModelService and Model interface
    - 🛠️ Create ModelService for managing motorcycle models
    - 🔍 Implement getModelsByBrand method to fetch models by brand ID
    - ➕ Add addModel method for creating new models
    - 📝 Create Model interface to define model structure
    - 🔄 Use signals for reactive model state management

---
+++ src/app/features/dashboard/motorcycles/services/model.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Model } from '../interfaces/model.interface';
+import { environment } from '../../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModelService {
+  private readonly _http = inject(HttpClient);
+  private readonly _models = signal<Model[]>([]);
+
+  get models() {
+    return this._models.asReadonly();
+  }
+
+  getModelsByBrand(brandId: number): Observable<Model[]> {
+    return this._http.get<Model[]>(
+      `${environment.API_URL}/motorcycle/model/brand/${brandId}`
+    );
+  }
+
+  addModel(brandId: number, name: string): Observable<Model> {
+    return this._http.post<Model>(`${environment.API_URL}/motorcycle/model`, {
+      brandId,
+      name,
+    });
+  }
+}
