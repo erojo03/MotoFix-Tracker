f5b54db8baca4aa9fa35c90b196e3087e2e49237
erojo03 <edgar.rojo03@outlook.com>
2024-11-09 17:08:22 -0500

    refactor: Enhance filter pipe with nested property search
    - Changed searchFields parameter to string[] to support multiple fields
    - Implemented getNestedPropertyValue method for accessing nested object properties
    - Updated fieldValue retrieval to use getNestedPropertyValue method
    
    This change allows the filter pipe to search through nested object properties,
    providing more flexibility in filtering complex data structures.

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -6,19 +6,35 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class FilterPipe implements PipeTransform {
-
-  transform<T>(entities: T[] | null, entityType: string, searchTerm: string, searchFields: (keyof T)[]): T[] {
+  transform<T>(
+    entities: T[] | null,
+    searchTerm: string,
+    searchFields: string[]
+  ): T[] {
     if (!entities || !searchTerm) {
       return entities || [];
     }
 
     searchTerm = searchTerm.toLowerCase();
 
-    return entities.filter(entity => {
-      return searchFields.some(field => {
-        const fieldValue = String(entity[field]).toLowerCase();
-        return fieldValue.includes(searchTerm);
+    return entities.filter((entity) => {
+      return searchFields.some((field) => {
+        const fieldValue = this.getNestedPropertyValue(entity, field);
+        return (
+          fieldValue && String(fieldValue).toLowerCase().includes(searchTerm)
+        );
       });
     });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private getNestedPropertyValue(obj: any, path: string) {
+    return path
+      .split('.')
+      .reduce(
+        (current, key) =>
+          current && current[key] !== undefined ? current[key] : null,
+        obj
+      );
   }
 }
