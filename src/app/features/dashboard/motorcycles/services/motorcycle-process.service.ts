29390a3fa1774754c7c5b37e81d407e3c1c4249c
erojo03 <edgar.rojo03@outlook.com>
2024-11-11 16:23:37 -0500

    feat(motorcycleProcess): update motorcycle list after fetching processes
    - Add call to refresh motorcycle list when processes are fetched
    - Ensure motorcycle data is synchronized with process updates

--- src/app/features/dashboard/motorcycles/services/motorcycle-process.service.ts
+++ src/app/features/dashboard/motorcycles/services/motorcycle-process.service.ts
@@ -3,12 +3,14 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../../../environments/environment.development';
 import { Observable, tap } from 'rxjs';
 import { MotorcycleProcess } from '../interfaces/process.interface';
+import { MotorcycleService } from './motorcycle.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MotorcycleProcessService {
   private _http = inject(HttpClient);
+  private _motorcycleService = inject(MotorcycleService);
   private readonly _processes = signal<MotorcycleProcess[]>([]);
 
   get processes() {
@@ -18,7 +20,12 @@ export class MotorcycleProcessService {
   getProcesses(motoId: number): Observable<MotorcycleProcess[]> {
     return this._http
       .get<MotorcycleProcess[]>(`${environment.API_URL}/moto-process/${motoId}`)
-      .pipe(tap((processes) => this._processes.set(processes)));
+      .pipe(
+        tap((processes) => {
+          this._processes.set(processes);
+          this._motorcycleService.getMotos().subscribe();
+        })
+      );
   }
 
   completeProcess(
